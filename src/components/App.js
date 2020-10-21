fbe4978cd2a5ce6f46c21a1088d6f625325beeb7
jatinvyas1 <jatin.vyas@outlook.com>
2020-10-21 20:42:13 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,18 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [showContent,setContent] = React.useState(false);
+  let showPara = () =>{
+    setContent(true);
+  }
+
   return (
     <div id="main">
       // Do not alter the main div
+      <button id="click" onClick={showPara}></button>
+      {
+        showContent?<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>:null
+      }
     </div>
   );
 }
